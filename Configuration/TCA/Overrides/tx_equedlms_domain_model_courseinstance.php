199bcbe7dd108fb86191b8f52acfea17b96ba921
Matt <matthew.scharf.ms@gmail.com>
2025-04-18 20:45:42 +0200

    TCA1

--- Configuration/TCA/Overrides/tx_equedlms_domain_model_courseinstance.php
+++ Configuration/TCA/Overrides/tx_equedlms_domain_model_courseinstance.php
@@ -1,1 +1,90 @@
-<?php // TCA CourseInstance
+
+<?php
+defined('TYPO3') or die();
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+(function () {
+    ExtensionManagementUtility::addTCAcolumns('tx_equedlms_domain_model_courseinstance', [
+        'program' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseinstance.program',
+            'config' => [
+                'type' => 'group',
+                'internal_type' => 'db',
+                'allowed' => 'tx_equedlms_domain_model_courseprogram',
+                'foreign_table' => 'tx_equedlms_domain_model_courseprogram',
+                'maxitems' => 1,
+                'minitems' => 1,
+            ],
+        ],
+        'training_center' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseinstance.training_center',
+            'config' => [
+                'type' => 'input',
+            ],
+        ],
+        'instructor' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseinstance.instructor',
+            'config' => [
+                'type' => 'group',
+                'internal_type' => 'db',
+                'allowed' => 'fe_users',
+                'maxitems' => 1,
+                'minitems' => 0,
+            ],
+        ],
+        'start_date' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseinstance.start_date',
+            'config' => [
+                'type' => 'datetime',
+                'required' => true,
+            ],
+        ],
+        'end_date' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseinstance.end_date',
+            'config' => [
+                'type' => 'datetime',
+                'required' => true,
+            ],
+        ],
+        'external_examiner_required' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseinstance.external_examiner_required',
+            'config' => [
+                'type' => 'check',
+                'items' => [
+                    ['LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.enabled', 1]
+                ]
+            ],
+        ],
+        'examiner_assigned' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseinstance.examiner_assigned',
+            'config' => [
+                'type' => 'group',
+                'internal_type' => 'db',
+                'allowed' => 'fe_users',
+                'maxitems' => 1,
+            ],
+        ],
+        'status' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseinstance.status',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    ['-- Label --', ''],
+                    ['Planned', 'planned'],
+                    ['Active', 'active'],
+                    ['Finished', 'finished'],
+                    ['Cancelled', 'cancelled'],
+                ],
+                'default' => 'planned'
+            ],
+        ],
+    ]);
+
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'tx_equedlms_domain_model_courseinstance',
+        '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+         program, training_center, instructor, start_date, end_date, external_examiner_required, examiner_assigned, status'
+    );
+})();
--- Configuration/TCA/Overrides/tx_equedlms_domain_model_courseprogram.php
+++ Configuration/TCA/Overrides/tx_equedlms_domain_model_courseprogram.php
@@ -1,1 +1,74 @@
-<?php // TCA CourseProgram
+
+<?php
+defined('TYPO3') or die();
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+(function () {
+    ExtensionManagementUtility::addTCAcolumns('tx_equedlms_domain_model_courseprogram', [
+        'title' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseprogram.title',
+            'config' => [
+                'type' => 'input',
+                'required' => true,
+            ],
+        ],
+        'identifier' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseprogram.identifier',
+            'config' => [
+                'type' => 'input',
+                'required' => true,
+                'eval' => 'unique'
+            ],
+        ],
+        'description' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseprogram.description',
+            'config' => [
+                'type' => 'text',
+                'enableRichtext' => true,
+                'richtextConfiguration' => 'default',
+                'cols' => 40,
+                'rows' => 10,
+            ],
+        ],
+        'certification_goal' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseprogram.certification_goal',
+            'config' => [
+                'type' => 'input',
+            ],
+        ],
+        'prerequisites' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseprogram.prerequisites',
+            'config' => [
+                'type' => 'text',
+                'rows' => 5,
+                'cols' => 40,
+                'eval' => 'trim'
+            ],
+        ],
+        'is_specialty' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseprogram.is_specialty',
+            'config' => [
+                'type' => 'check',
+                'items' => [
+                    ['LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.enabled', 1]
+                ]
+            ],
+        ],
+        'requires_validation' => [
+            'label' => 'LLL:EXT:equed_lms/Resources/Private/Language/locallang_db.xlf:courseprogram.requires_validation',
+            'config' => [
+                'type' => 'check',
+                'items' => [
+                    ['LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.enabled', 1]
+                ]
+            ],
+        ],
+    ]);
+
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'tx_equedlms_domain_model_courseprogram',
+        '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+         title, identifier, description, certification_goal, prerequisites, is_specialty, requires_validation'
+    );
+})();
